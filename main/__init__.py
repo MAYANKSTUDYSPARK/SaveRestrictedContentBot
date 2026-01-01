b1b6fe419dd123047a62b32123a51b758507e1ac
MAYANKSTUDYSPARK <vermamayank83350s@gmail.com>
2026-01-01 12:58:29 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = config("22657422", default=None, cast=int)
+API_HASH = config("354881b0718d6f5439d8a523019ffd2e", default=None)
+BOT_TOKEN = config("8284935484:AAFo0R7IzQb8icullxEwWtA9MS7X7jomwpk", default=None)
+SESSION = config("BQFZuY4ALaSrRJKPtxv_eC8RD3c961-xAOBNdqCKR6Ou0dQfATf3U3V46U41dhJ16Eh1H48D66S6HeBZtA51-XqO0xjE8DmkfPqy4nwd4hgwtexneqltK65e9VpI2uH_dS-37JeOT1KebmUGMK9PkBu_c4Tz5pckQRrVRElc8hrzr46FNWIeIEfb7AzBbmc4T7EysP8MDo6No6Gvoh3oOTaR1JOJFasEzlsZ9VXH8E55LKG-6GrSa_WcAYtqMf-DOimbjo8QHR9iGOrSs_fKPUsCE76acM2xHvVdW3LjKhwV4smaEacxU7vNH0-Hlc-JS6mBkqgCjMikT_iPIkSa049oXs6OhQAAAAHp5pqMAA", default=None)
+FORCESUB = config("scholarversepro_network", default=None)
+AUTH = config("8219171468", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
